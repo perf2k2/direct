1867575468f02a35a6b7e038d8b91c3ec475dc3d
Perf2k2 <perf2k2@gmail.com>
2016-06-10 14:38:45 +0300

    Added exchange rates

--- src/dictionaries/Currency.php
+++ src/dictionaries/Currency.php
@@ -5,4 +5,11 @@ namespace perf2k2\direct\dictionaries;
 class Currency
 {
     public static $multiplier = 1000000;
+    public static $exchangeRateRUB = 30;
+    public static $exchangeRateUSD = 0.41;
+    public static $exchangeRateCHF = 0.41;
+    public static $exchangeRateEUR = 0.39;
+    public static $exchangeRateKZT = 117.6;
+    public static $exchangeRateTRY = 1.2626;
+    public static $exchangeRateUAH = 12;
 }
