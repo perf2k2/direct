e285e3fb34dfa6881d89b2661a0376b54ffddf97
Perf2k2 <perf2k2@gmail.com>
2018-03-01 23:11:32 +0300

    New tests

--- tests/acceptance/LiveApiTest.php
+++ tests/acceptance/LiveApiTest.php
@@ -2,6 +2,7 @@
 
 namespace acceptance;
 
+use api\entities\adgroups\AdGroupAddItem;
 use api\entities\campaigns\CampaignAddItem;
 use api\entities\campaigns\textcampaign\StrategyAverageCpcAdd;
 use api\entities\campaigns\textcampaign\StrategyNetworkDefaultAdd;
@@ -11,6 +12,8 @@ use api\entities\campaigns\textcampaign\TextCampaignSearchStrategyAdd;
 use api\entities\campaigns\textcampaign\TextCampaignStrategyAdd;
 use api\enums\campaign\textcampaign\TextCampaignNetworkStrategyTypeEnum;
 use api\enums\campaign\textcampaign\TextCampaignSearchStrategyTypeEnum;
+use perf2k2\direct\AdGroups;
+use perf2k2\direct\api\entities\IdsCriteria;
 use perf2k2\direct\Campaigns;
 use perf2k2\direct\credentials\ConfigFileCredential;
 use perf2k2\direct\http\Connection;
@@ -36,7 +39,58 @@ class LiveApiTest extends \PHPUnit_Framework_TestCase
             ])
             ->createAndSendRequest(new Connection(new ConfigFileCredential(__DIR__ . '/../../'), 'ru', true));
         
+        $campaignId = $response->getResult('AddResults')[0]->Id;
         $this->assertInstanceOf(Response::class, $response);
-        $this->assertTrue(is_numeric($response->getResult('AddResults')[0]->Id));
+        $this->assertTrue(is_numeric($campaignId));
+        
+        return $campaignId;
+    }
+    
+    /**
+     * @depends testAddCampaign
+     */
+    public function testAddGroup(int $campaignId)
+    {
+        $response = AdGroups::add()
+            ->setAdGroups([
+                new AdGroupAddItem('Test group', $campaignId, [1])
+            ])
+            ->createAndSendRequest(new Connection(new ConfigFileCredential(__DIR__ . '/../../'), 'ru', true));
+        
+        $adGroupId = $response->getResult('AddResults')[0]->Id;
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertTrue(is_numeric($adGroupId));
+        
+        return $adGroupId;
+    }
+    
+    /**
+     * @depends testAddGroup
+     */
+    public function testDeleteGroup(int $adGroupId)
+    {
+        $response = AdGroups::delete()
+            ->setSelectionCriteria(
+                (new IdsCriteria())->setIds([$adGroupId])
+            )
+            ->createAndSendRequest(new Connection(new ConfigFileCredential(__DIR__ . '/../../'), 'ru', true));
+        
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals($adGroupId, $response->getResult('DeleteResults')[0]->Id);
+    }
+    
+    /**
+     * @depends testAddCampaign
+     */
+    public function testDeleteCampaign(int $campaignId)
+    {
+        $response = Campaigns::delete()
+            ->setSelectionCriteria(
+                (new IdsCriteria())->setIds([$campaignId])
+            )
+            ->createAndSendRequest(new Connection(new ConfigFileCredential(__DIR__ . '/../../'), 'ru', true));
+        
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals($campaignId, $response->getResult('DeleteResults')[0]->Id);
     }
 }
