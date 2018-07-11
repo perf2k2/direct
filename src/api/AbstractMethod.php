c6e926982efe889df25d935e14f2c21909844440
Perf2k2 <perf2k2@gmail.com>
2018-07-11 19:06:24 +0300

    Unused code was removed

--- src/api/AbstractMethod.php
+++ src/api/AbstractMethod.php
@@ -2,11 +2,6 @@
 
 namespace direct\api;
 
-use direct\http\Connection;
-use direct\http\Params;
-use direct\http\Request;
-use direct\http\Response;
-
 abstract class AbstractMethod implements ApiObjectInterface, ApiParametrizedObjectInterface
 {
     protected $serviceName;
@@ -19,25 +14,6 @@ abstract class AbstractMethod implements ApiObjectInterface, ApiParametrizedObje
     
     abstract public function getApiName(): string;
 
-    public function createRequest(Connection $connection): Request
-    {
-        return $connection->createRequest()
-            ->setService($this->serviceName)
-            ->setMethod($this->getApiName())
-            ->setParameters(new Params($this->getData()));
-    }
-
-    public function sendRequest(Connection $connection, Request $request): Response
-    {
-        return $connection->sendRequest($request);
-    }
-
-    public function createAndSendRequest(Connection $connection): Response
-    {
-        $request = $this->createRequest($connection);
-        return $this->sendRequest($connection, $request);
-    }
-    
     public function getServiceName(): string
     {
         return $this->serviceName;
