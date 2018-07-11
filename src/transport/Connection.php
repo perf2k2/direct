74cd718c5bdfc283c217d17a36433df118ee7c55
Perf2k2 <perf2k2@gmail.com>
2018-07-11 18:11:31 +0300

    Using Guzzle for work with http

--- src/transport/Connection.php
+++ src/transport/Connection.php
@@ -7,6 +7,31 @@ class Connection
 {
     public function send(Request $request): Response
     {
-        return new Response(1, '1/2/3');
+        $httpClient = new \GuzzleHttp\Client(['base_uri' => 'https://api.direct.yandex.com/json/v5/']);
+        $httpResponse = $httpClient->post($request->getService(), [
+            'headers' => [
+                'Authorization' => "Bearer {$request->getToken()}",
+                'Accept-Language' => $request->getLanguage(),
+                'Client-Login' => $request->getLogin(),
+                'Content-Type' => 'application/json; charset=utf-8',
+            ],
+            'json' => [
+                'method' => $request->getMethod(),
+                'params' => $request->getParams(),
+            ],
+        ]);
+        
+        if ($httpResponse->hasHeader('Units')) {
+            return new Response(
+                (int) $httpResponse->getHeader('RequestId')[0],
+                (string) $httpResponse->getBody()->getContents(),
+                (string) $httpResponse->getHeader('Units')[0]
+            );
+        }
+        
+        return new Response(
+            (int) $httpResponse->getHeader('RequestId')[0],
+            (string) $httpResponse->getBody()->getContents()
+        );
     }
 }
