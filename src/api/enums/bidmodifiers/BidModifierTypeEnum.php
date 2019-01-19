d46b8b59551b896e1436a8235edd16e58e9db3bb
Romannikov Nikita <perf2k2@gmail.com>
2019-01-19 16:21:26 +0300

    Actual bid modifier types

--- src/api/enums/bidmodifiers/BidModifierTypeEnum.php
+++ src/api/enums/bidmodifiers/BidModifierTypeEnum.php
@@ -6,12 +6,18 @@ use perf2k2\direct\api\Enum;
 
 /**
  * @method static BidModifierTypeEnum MOBILE_ADJUSTMENT()
+ * @method static BidModifierTypeEnum DESKTOP_ADJUSTMENT()
  * @method static BidModifierTypeEnum DEMOGRAPHICS_ADJUSTMENT ()
  * @method static BidModifierTypeEnum RETARGETING_ADJUSTMENT()
+ * @method static BidModifierTypeEnum REGIONAL_ADJUSTMENT()
+ * @method static BidModifierTypeEnum VIDEO_ADJUSTMENT()
  */
 final class BidModifierTypeEnum extends Enum
 {
     const MOBILE_ADJUSTMENT = 'MOBILE_ADJUSTMENT';
+    const DESKTOP_ADJUSTMENT = 'DESKTOP_ADJUSTMENT';
     const DEMOGRAPHICS_ADJUSTMENT  = 'DEMOGRAPHICS_ADJUSTMENT ';
     const RETARGETING_ADJUSTMENT = 'RETARGETING_ADJUSTMENT';
+    const REGIONAL_ADJUSTMENT = 'REGIONAL_ADJUSTMENT';
+    const VIDEO_ADJUSTMENT = 'VIDEO_ADJUSTMENT';
 }
