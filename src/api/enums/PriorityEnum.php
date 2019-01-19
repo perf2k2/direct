b4bc61f67f4562bf3c601c0a4f738078cccfe653
Romannikov Nikita <perf2k2@gmail.com>
2019-01-19 17:11:13 +0300

    Bugfix

--- src/api/enums/PriorityEnum.php
+++ src/api/enums/PriorityEnum.php
@@ -2,12 +2,14 @@
 
 namespace perf2k2\direct\api\enums;
 
+use perf2k2\direct\api\Enum;
+
 /**
  * @method static PriorityEnum LOW()
  * @method static PriorityEnum NORMAL()
  * @method static PriorityEnum HIGH()
  */
-final class PriorityEnum
+final class PriorityEnum extends Enum
 {
     const LOW = 'LOW';
     const NORMAL = 'NORMAL';
